f02950c956215f5367da7d0a5c94339d6f091d33
Kurt Blair <kurt.blair@queensu.ca>
2019-06-30 18:36:54 -0400

    Update Assignment.sql

--- Assignment.sql
+++ Assignment.sql
@@ -1,103 +1,52 @@
-/*Question1:*/
- 
-SELECT fname, lname, ssn 
-FROM employee, department
-WHERE (SSN = MGRSSN AND employee.Sex= 'M' AND Salary > 30000)
+--Question 1
 
-/*Question2:*/
+SELECT fname, lname, ssn, salary
+FROM employee
+WHERE sex = "M" AND salary > 30000
 
-SELECT DISTINCT essn 
-FROM project, works_on 
-WHERE (project.PNumber = works_on.PNo 
-AND (project.Plocation = 'Stafford' OR project.PLocation='Sugarland'))
-ORDER BY eSSN DESC
+--Question 2
 
-/*Question3:*/
+SELECT SSN 
+FROM Employee e JOIN DeptLocations d ON (e.dno = d.dnumber)
+WHERE d.dlocation = 'Sugarland' OR d.dlocation = 'Stafford'
+ORDER BY SSN DESC;
 
+--Question 3
 
-SELECT Fname, Lname, dependent_name
-FROM employee, dependent
-WHERE (ssn = essn AND (dependent.Relationship = 'Son' OR dependent.Relationship = 'Daughter')) 
+SELECT fname, lname, dependent_name
+FROM employee e JOIN dependent d ON (e.ssn = d.essn)
+WHERE d.relationship <> 'SPOUSE';
 
+--Question 4
 
+SELECT e.Fname, m.lname
+FROM Employee e JOIN (SELECT lname, SSN FROM Department d JOIN Employee m ON (d.MGRSSN = m.SSN) ) m
+WHERE m.ssn = e.superSSN
 
-/*Question 4:*/
+--Question 5
 
-CREATE TEMPORARY TABLE managers(
-        MSSN		CHAR(11)	NOT NULL,
-        Mlname	VARCHAR(20)	NOT NULL,
-        PRIMARY KEY(MSSN)
-    );
-INSERT INTO managers 
-(SELECT department.MGRSSN AS MSSN, employee.LName AS Mlname
-FROM department, employee
-WHERE department.MGRSSN = employee.SSN
-);
-(SELECT employee.LName, managers.Mlname
-FROM employee, managers
- WHERE employee.SuperSSN = managers.MSSN
-);
-
-/*Question 5:*/
-
-CREATE TEMPORARY TABLE FrankProjects(
-        fSSN		CHAR(11)	NOT NULL,
-        Projname	VARCHAR(15)	NOT NULL,
-        pnum int NOT NULL,
-        PRIMARY KEY(pnum)
-    );
-INSERT INTO FrankProjects 
-(SELECT works_on.ESSN AS fSSN, project.PName AS Projname, project.PNumber as pnum
-FROM works_on, project
-WHERE works_on.PNo = project.PNumber AND ESSN = '333445555'
-);
-
-CREATE TEMPORARY TABLE AliciaProjects(
-        fSSN		CHAR(11)	NOT NULL,
-        Proname	VARCHAR(15)	NOT NULL,
-        pnu int NOT NULL,
-        PRIMARY KEY(pnu)
-    );
-INSERT INTO AliciaProjects 
-(SELECT works_on.ESSN AS fSSN, project.PName AS Proname, project.PNumber as pnu
-FROM works_on, project
-WHERE works_on.PNo = project.PNumber AND ESSN = '999887777'
-);
-
-(SELECT Proname, pnu
- FROM AliciaProjects,FrankProjects
- WHERE AliciaProjects.pnu = FrankProjects.pnum)
- 
-/*Question */
-
-SELECT DISTINCT
-    lname,
-    SSN
+SELECT p.pno,v.pname
 FROM
-    employee,
-    works_on
-GROUP BY
-    lname,
-    essn
-HAVING
-    COUNT(*) = 2
-	
-/*Question 7	*/
+(SELECT pno FROM employee e JOIN Works_On w ON (e.ssn = w.essn)
+ WHERE e.fname = 'Franklin') p
+ JOIN
+ (SELECT pno FROM employee z JOIN Works_On b ON (z.ssn = b.essn) 
+ WHERE z.fname = 'Alicia') c
+ ON (p.pno = c.pno) JOIN Project v ON (p.pno = v.pnumber)
 
-CREATE TEMPORARY TABLE hourly(
-        esn		CHAR(11)	NOT NULL,
-        hours int NOT NULL,
-        numprojs int NOT NULL,
-        Salary int NOT NULL,
-        PRIMARY KEY(esn)
-    );
+--Question 6
 
-INSERT INTO hourly 
-(SELECT ESSN, SUM(Hours),COUNT(Hours),Salary
-FROM works_on,employee
-GROUP BY ESSN
-);
+SELECT e.ssn, e.lname, COUNT(w.pno) as "NUM"
+FROM employee e JOIN works_on w ON (e.ssn = w.essn)
+group by e.ssn
+HAVING COUNT(w.pno) = 2
+;
 
-(SELECT esn, numprojs, Salary/(52*hours) as HourlyWage
- FROM hourly)
- 
+--Question 7
+
+SELECT ssn,salary/(SUM(hours)*52) AS WAGE
+FROM employee e JOIN works_on w ON (e.ssn = w.essn)
+GROUP BY ssn
+ORDER BY WAGE ASC;
+
+
